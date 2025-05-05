ffd13accf92c862beb44715a5ed58a6c1b0e198c
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-05 11:22:02 +0700

    setNama beb

---
+++ friendFuctionSatu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+    private:
+        string nama;
+    public:
+        friend void setNama(mahasiswa &a, string);
+};
+
+
+void setNama(mahasiswa &a, string b){
+    a.nama = b; // Set the name of the mahasiswa object
+    cout << a.nama
+}
