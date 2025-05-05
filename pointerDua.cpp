4ba789566afee399e032d9e7921ef93bdcc84f48
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-05 10:51:22 +0700

    isi main guys

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -10,5 +10,10 @@ class mahasiswa {
 };
 
 int main(){
-    
+    mahasiswa *mhs = new mahasiswa; // Pointer to class object
+    mhs->nim = 1234567890; // Accessing class member using pointer
+    mhs->showNim(); // Calling class method using pointer
+
+    delete mhs; // Freeing allocated memory
+    return 0;
 }
