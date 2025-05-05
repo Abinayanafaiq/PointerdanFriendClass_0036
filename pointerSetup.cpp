c35dc283709b12850fb8cf32dea07986d63cb6f6
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-05 10:44:47 +0700

    nambahin int main

--- pointerSetup.cpp
+++ pointerSetup.cpp
@@ -6,5 +6,19 @@ class mahasiswa {
         int nim;
         void showNim(){
             cout << "No induk mahasiswa: " << nim << endl;
-        }
+        };
+};
+
+int main(){
+    mahasiswa mhs{1};
+    mhs.showNim();
+
+    mahasiswa &refMhs = mhs; // reference to mhs
+    refMhs.nim = 2; // change nim using reference   
+    mhs.showNim(); // show updated nim
+
+    mahasiswa *pMhs = &mhs; // pointer to mhs
+    pMhs->nim = 3; // change nim using pointer
+    pMhs->showNim(); // show updated nim
+    return 0;
 }
