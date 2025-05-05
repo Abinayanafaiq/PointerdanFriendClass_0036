8318622d76ec31af3615ca957abd2d978c899ce3
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-05 11:33:51 +0700

    class pelajar

---
+++ friendFunctionDua.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+class pelajar;
+class manusia
+{
+    public:
+        void showNilaiPelajr(pelajar &x); // Method to show nilai from pelajar class    
+
+};
+
+
+class pelajar
+{
+    private:
+        int nilai;
+    public:
+        pelajar() { nilai = 100; } // Constructor to initialize nilai
+        friend void manusia::showNilaiPelajr(pelajar &x); // Declare showNilaiPelajr as a friend of pelajar class
+};
